a90050f835a8284a5e39a7654b6eea0bd9fc607e
Sakshikaradi <sakshiakardi@gmail.com>
2025-10-14 19:40:28 +0530

    Update 5implement.py

--- 5implement.py
+++ 5implement.py
@@ -17,25 +17,28 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 def load_model():
     model_path = "model_finetuned.pth"
 
-    # ✅ Download from Google Drive using gdown (handles big files properly)
+    # Download if missing
     if not os.path.exists(model_path):
         try:
             import gdown
         except ImportError:
             os.system("pip install gdown")
             import gdown
-
         url = "https://drive.google.com/uc?id=1b2MVNoOAKrkV9wO4amuWPj4BTH3LvlY0"
         st.info("📥 Downloading model from Google Drive...")
         gdown.download(url, model_path, quiet=False)
         st.success("✅ Model downloaded successfully!")
 
-    # ✅ Load model
+    # Load model architecture
     model = models.resnet18(weights=None)
     model.fc = nn.Linear(model.fc.in_features, 2)
-    model.load_state_dict(torch.load(model_path, map_location="cpu"))
+
+    # ✅ Fix for PyTorch ≥2.6
+    model.load_state_dict(torch.load(model_path, map_location="cpu", weights_only=False))
+
     model.eval()
     return model
+
 
 
 # Load the model
@@ -69,3 +72,4 @@ if uploaded_file:
         _, predicted = torch.max(output, 1)
         classes = ["Normal", "Pneumonia"]
         st.subheader(f"🩺 Prediction: **{classes[predicted.item()]}**")
+
